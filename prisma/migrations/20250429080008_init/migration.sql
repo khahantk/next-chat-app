86a99fe66d26fda380109205776f5dd8cd08fb46
khahantk <chung.gkh@gmail.com>
2025-04-29 15:01:10 +0700

    Initial commit of final DB schema and migrations

--- prisma/migrations/20250429080008_init/migration.sql
+++ prisma/migrations/20250429080008_init/migration.sql
@@ -34,8 +34,11 @@ CREATE TABLE "users" (
     "password" TEXT,
     "role" TEXT NOT NULL DEFAULT 'user',
     "address" JSONB DEFAULT [],
+    "lastPing" DATETIME,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "updatedAt" DATETIME NOT NULL,
+    "isOnline" BOOLEAN NOT NULL DEFAULT false,
+    "lastSeen" DATETIME
 );
 
 -- CreateTable
@@ -57,7 +60,6 @@ CREATE TABLE "products" (
     "stock" INTEGER NOT NULL DEFAULT 0,
     "originalPrice" DECIMAL NOT NULL DEFAULT 0,
     "price" DECIMAL NOT NULL DEFAULT 0,
-    "author" TEXT NOT NULL,
     "rating" DECIMAL NOT NULL DEFAULT 0,
     "numReviews" INTEGER NOT NULL DEFAULT 0,
     "isFeatured" BOOLEAN NOT NULL DEFAULT false,
@@ -68,31 +70,46 @@ CREATE TABLE "products" (
 );
 
 -- CreateTable
-CREATE TABLE "chat" (
+CREATE TABLE "channels" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "productId" TEXT NOT NULL,
+    "name" TEXT,
+    "isGroup" BOOLEAN NOT NULL DEFAULT false,
+    "productId" TEXT,
+    "ownerId" TEXT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    CONSTRAINT "chat_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "channels_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "channels_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "users" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "channel_members" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "channelId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "joinedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "channel_members_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "channels" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "channel_members_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "messages" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "chatId" TEXT NOT NULL,
+    "channelId" TEXT NOT NULL,
     "senderId" TEXT NOT NULL,
     "content" TEXT NOT NULL,
-    "sentAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "messages_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "chat" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "messages_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "channels" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "messages_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
-CREATE TABLE "_UserChats" (
-    "A" TEXT NOT NULL,
-    "B" TEXT NOT NULL,
-    CONSTRAINT "_UserChats_A_fkey" FOREIGN KEY ("A") REFERENCES "chat" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "_UserChats_B_fkey" FOREIGN KEY ("B") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE "ChannelReadState" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "channelId" TEXT NOT NULL,
+    "lastReadAt" DATETIME NOT NULL,
+    CONSTRAINT "ChannelReadState_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "ChannelReadState_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "channels" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateIndex
@@ -108,7 +125,7 @@ CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 CREATE UNIQUE INDEX "verification_tokens_identifier_token_key" ON "verification_tokens"("identifier", "token");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "_UserChats_AB_unique" ON "_UserChats"("A", "B");
+CREATE UNIQUE INDEX "channel_members_channelId_userId_key" ON "channel_members"("channelId", "userId");
 
 -- CreateIndex
-CREATE INDEX "_UserChats_B_index" ON "_UserChats"("B");
+CREATE UNIQUE INDEX "ChannelReadState_channelId_userId_key" ON "ChannelReadState"("channelId", "userId");
