53f7c068afc44e366fb86def40b5e3fff09c6fe3
khahantk <chung.gkh@gmail.com>
2025-04-29 17:58:34 +0700

    Update seed data

--- src/db/sample-data.ts
+++ src/db/sample-data.ts
@@ -1,26 +1,27 @@
 import { hashSync } from 'bcryptjs'
+import slugify from 'slugify'
 const sampleData = {
   users: [
     {
-      name: 'Chung',
+      name: 'Chung Nguyen',
       email: 'chung.gkh@gmail.com',
       password: hashSync('123456', 10),
       role: 'user'
     },
     {
-      name: 'Khanh',
+      name: 'Khanh Nguyen',
       email: 'khanh@gmail.com',
       password: hashSync('123456', 10),
       role: 'user'
     },
     {
-      name: 'Han',
+      name: 'Han Nguyen',
       email: 'han@gmail.com',
       password: hashSync('123456', 10),
       role: 'user'
     },
     {
-      name: 'Hien',
+      name: 'Hien Nguyen',
       email: 'hien@gmail.com',
       password: hashSync('123456', 10),
       role: 'user'
@@ -37,12 +38,22 @@ const sampleData = {
       title: 'The Complete Developer: Master the Full Stack with TypeScript, React, Next.js',
       slug: 'the-complete-developer-master-the-full-stack-with-typescript-react-nextjs',
       description: 'Web / Web Services & APIs, Web / Web Programming, General, Programming Languages / Javascript',
-      category: 'category text',
-      brand: 'brand text',
-      author: 'author text',
+      category: 'book',
+      brand: '',
       price: 46.83,
-      sellerId: 'cm9jmoaca0003t0d6qliny59s'
-    }
+      images: ['/images/book-sample.webp'],
+      sellerId: 'cma2e319p0003t0ht5bghu8y9',
+    },
+    {
+      title: 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+      slug: slugify('Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones'),
+      description: 'No matter your goals, Atomic Habits offers a proven framework for improving--every day. James Clear, one of the world\'s leading experts on habit formation, reveals practical strategies that will teach you exactly how to form good habits, break bad ones, and master the tiny behaviors that lead to remarkable results',
+      category: 'book',
+      brand: '',
+      price: 45.99,
+      images: ['/images/auto-habits.jpg'],
+      sellerId: 'cma2e319p0003t0ht5bghu8y9',
+    },
   ]
 }
 
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -2,25 +2,33 @@ import { PrismaClient } from '@prisma/client';
 import sampleData from "./sample-data";
 
 const prisma = new PrismaClient();
-
-async function seed() {
-  // await prisma.user.createMany({
-  //   data: sampleData.users
-  // });
+async function seedProducts() {
+  await prisma.product.deleteMany();
   await prisma.product.createMany({
     data: sampleData.products
   });
 }
+
+async function seedUsers() {
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
+
+  await prisma.user.createMany({
+    data: sampleData.users
+  });
+
+}
+
+async function seed() {
+  await seedUsers();
+  await seedProducts();
+
+}
 async function main() {
   try {
     console.log('-- run seed data --');
-    // await prisma.account.deleteMany();
-    // await prisma.session.deleteMany();
-    // await prisma.verificationToken.deleteMany();
-    // await prisma.user.deleteMany();
-
-    await prisma.product.deleteMany();
-    
     await seed();
     await prisma.$disconnect();
     console.log('-- run seed data successful --');
